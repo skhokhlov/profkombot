27f6233ba6422b14ebb2106336902cd5369e45d3
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2018-12-19 21:36:50 +0300

    Release 1.0

--- bot/bot.js
+++ bot/bot.js
@@ -137,27 +137,100 @@ bot.use((ctx, next) => {
     })
 });
 
-bot.command('start', (ctx) => requestContact(ctx.reply));
+// bot.command('start', (ctx) => requestContact(ctx.reply));
+bot.command('start', (ctx => ctx.reply("Бот Профокма студентов РТУ МИРЭА. Бот работает в тестовом режиме.", Extra.markup((markup) => {
+    return markup.resize()
+        .keyboard([
+            markup.callbackButton('Контакты отделений', 'Контакты отделений'),
+            markup.callbackButton('Часы работы', 'Часы работы'),
+            markup.contactRequestButton('Отправить контакт')
+        ])
+}))));
 
-bot.on('contact', ({reply, session, message}) => {
-    session.userPhone = session.userPhone || null;
-    session.requestCounter = session.requestCounter | 0;
-
-    if (session.userPhone !== message.contact.phone_number && session.requestCounter <= 3) {
-        session.userPhone = Number(message.contact.phone_number.replace('+', '').replace(/^8/, 7));
-        session.requestCounter++;
-    }
-
-    if (!(session.requestCounter <= 3 || session.userPhone === message.contact.phone_number)) {
-        return reply('Превышен лимит запросов разных номеров');
-    } else {
-        return reply('Теперь вы можете проверить наличие в базе', Extra.markup((markup) =>
-            markup.resize().keyboard([
-                markup.callbackButton('РЖД бонус', 'ржд бонус')
-            ])
-        ));
-    }
-});
+bot.on('contact', ({reply, session, message}) => { reply("На данный момент эта функция недоступна. " +
+    "В будещем по номеру телефона можно будет опредлять стаутс заявки " +
+    "РЖД бонус и узнавать состояние заявления на дотации.") });
+// bot.on('contact', ({reply, session, message}) => {
+//     session.user = session.user || null;
+//     session.requestCounter = session.requestCounter || 0;
+//
+//     if (session.user.tel !== message.contact.phone_number && session.requestCounter <= 3) {
+//         session.user.tel = parseInt(message.contact.phone_number);
+//         session.requestCounter++;
+//
+//     }
+//
+//     if (session.user.tel == null) {
+//         sendError(reply);
+//         console.error('Value of session.userPhone is null');
+//
+//     } else {
+//         if (session.requestCounter > 3 || session.user.tel !== message.contact.phone_number) {
+//             return reply('Превышен лимит запросов разных номеров');
+//
+//         } else {
+//             request.get(`${api}chat/${message.chat.id}`, (err, res, body) => {
+//                 if (err) {
+//                     console.error(`${api}chat/${message.chat.id}\n ${err}`);
+//                     return sendError(reply);
+//                 }
+//
+//                 if (res.statusCode === 200) {
+//                     let data = JSON.parse(body);
+//
+//                 }
+//             });
+//         }
+//     }
+//
+//
+//     session.userPhone = session.userPhone || null;
+//     session.requestCounter = session.requestCounter || 0;
+//
+//     if (session.userPhone !== message.contact.phone_number && session.requestCounter <= 3) {
+//         session.userPhone = Number(message.contact.phone_number.replace('+', '').replace(/^8/, 7));
+//         session.requestCounter++;
+//
+//     }
+//
+//     if (session.userPhone == null) {
+//         sendError(reply);
+//         console.error('Value of session.userPhone is null');
+//
+//     } else {
+//         if (!(session.requestCounter <= 3 || session.userPhone === message.contact.phone_number)) {
+//             return reply('Превышен лимит запросов разных номеров');
+//         } else {
+//             request.get(`${api}chat/${message.chat.id}`, (err, res, body) => {
+//                 if (err) {
+//                     console.error(`${api}chat/${message.chat.id}\n ${err}`);
+//                     sendError(reply);
+//
+//                 }
+//
+//                 if (res.statusCode === 200) {
+//                     const data = JSON.parse(body);
+//                     if (data[1][0] === message.chat.id) { // надо еще проверять, что номер не  сохранен в сессии
+//                         if (data[1][1] === session.userPhone) { //
+//                             requestKeyboard(reply);
+//
+//                         } else {
+//                             updatePhone(reply, session, message);
+//
+//                         }
+//                     }
+//
+//                 } else if (res.statusCode === 404) {
+//                     updatePhone(reply, session, message);
+//
+//                 } else {
+//                     sendError(reply);
+//
+//                 }
+//             });
+//         }
+//     }
+// });
 
 bot.command('api', (ctx) => request(api, (error, res, body) => {
     return ctx.reply(body).catch(err => console.error(err));
@@ -167,17 +240,38 @@ bot.hears(/Мои данные/, ({reply, session}) => reply(session.userPhone |
 
 bot.hears(/(ржд|Ржд|РЖД) бонус/, ({match, reply, session}) => {
     if (session.userPhone != null) {
-        request(`${api}user/${session.userPhone}`, (error, res, body) => {
-            const data = JSON.parse(body);
-            if (data[0][0] === 'status' && data[0][1] === 'OK') {
-                reply(`${data[1][2]} есть в базе РЖД бонус`).catch(err => console.error(err));
-            } else {
-                reply('Произошла ошибка').catch(err => console.error(err));
+        request.get(`${api}user/${session.userPhone}`, (err, res, body) => {
+            if (err) {
+                console.error(`${api}chat/${message.chat.id}/tel/${session.userPhone}\n ${err}`);
+                sendError(reply);
+
             }
-        })
+
+            if (res.statusCode === 200) {
+                let data = JSON.parse(body);
+                reply(`${session.userPhone} + ${data[1][2]} есть в базе РЖД бонус`).catch(err => console.error(err));
+
+            } else {
+                sendError(reply);
+
+            }
+        });
     } else {
         requestContact(reply);
+
     }
+});
+
+bot.hears(/Контакты отделений/, ({match, reply, session}) => {
+    reply(`Отделения Профкома студентов находтся в трех кампусах университета:\n
+    Вернадского 78 ауд. А-170\n
+    Вернадского 86 ауд. Л-117\n
+    Стромынка 20 ауд. 411\n
+    `)
+});
+
+bot.hears(/Часы работы/, ({match, reply, session}) => {
+    reply(`Отделения Профкома студентов работают с 11 до 17 часов с понедельника по пятницу.`)
 });
 
 // Start polling
