372e299af26f264049e598a8b87efeed97ad137b
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2019-01-06 14:00:23 +0300

    Update menu

--- bot/bot.js
+++ bot/bot.js
@@ -153,7 +153,10 @@ bot.use((ctx, next) => {
 });
 
 // bot.command('start', (ctx) => requestContact(ctx.reply));
-bot.command('start', ({reply}) => reply('Бот Профокма студентов РТУ МИРЭА. Бот работает в тестовом режиме.'), mainMenu(reply));
+bot.command('start', ({reply}) => {
+    reply('Бот Профокма студентов РТУ МИРЭА. Бот работает в тестовом режиме.');
+    mainMenu(reply);
+});
 
 bot.on('contact', ({reply, session, message}) => {
     session.tel = makeUpTel(message.contact.phone_number);
