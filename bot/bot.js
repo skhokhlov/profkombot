301f0a5b8be275134c737c00f1273560c3eeba23
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2019-02-03 17:43:02 +0300

    bot.js clean up

--- bot/bot.js
+++ bot/bot.js
@@ -14,44 +14,6 @@ const sendError = (reply) => reply('Произошла ошибка').catch((err
 
 const makeUpTel = (tel) => parseInt(tel.replace(/^\+7/, 8).replace(/^7/, 8));
 
-class User {
-    get last_name() {
-        return this._last_name;
-    }
-
-    set last_name(value) {
-        this._last_name = value;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    get first_name() {
-        return this._first_name;
-    }
-
-    set first_name(value) {
-        this._first_name = value;
-    }
-
-    set tel(value) {
-        this._tel = parseInt(value);
-    }
-
-    get tel() {
-        return this._tel;
-    }
-
-    constructor(parsedData) {
-        // let parsedData = JSON.parse(data)[0];
-        this._id = parsedData.id;
-        this._tel = parseInt(parsedData.tel);
-        this._first_name = parsedData.first_name;
-        this._last_name = parsedData.last_name;
-    }
-}
-
 /**
  * Send keyboard to user for the phone number request
  * @param reply
@@ -96,52 +58,7 @@ function mainMenu(reply) {
         }));
 }
 
-/**
- * Update phone number associated with user and send requestKeyboard()
- * @param reply
- * @param session
- * @param message
- */
-function updatePhone(reply, session, message) {
-    request.post(`${api}chat/${message.chat.id}/tel/${session.user.tel}`, (err, res, body) => {
-        if (err) {
-            console.error(`${api}chat/${message.chat.id}/tel/${session.user.tel}\n ${err}`);
-            return sendError(reply);
-
-        }
-
-        if (res.statusCode === 200) {
-            return requestKeyboard(reply);
-
-        } else {
-            console.error(`Response code of ${api}chat/${message.chat.id}/tel/${session.user.tel} is not 200`);
-            return sendError(reply);
-
-        }
-    });
-}
-
-function getUser(reply, session, message) {
-    request.get(`${api}user/${session.user.tel}`, (err, res, body) => {
-        if (err) {
-            console.error(`${api}chat/${message.chat.id}/tel/${session.user.tel}\n ${err}`);
-            return sendError(reply);
-
-        }
-
-        if (res.statusCode === 200) {
-            let data = JSON.parse(body)[0];
-            session.user = new User(data);
-            return reply(`${session.user.tel} + ${session.user.first_name} есть в базе РЖД бонус`).catch(err => console.error(err));
-
-        } else {
-            return sendError(reply);
-
-        }
-    });
-}
-
-// // Register session middleware
+// Register session middleware
 bot.use(session());
 
 // Register logger middleware
@@ -153,7 +70,6 @@ bot.use((ctx, next) => {
     })
 });
 
-// bot.command('start', (ctx) => requestContact(ctx.reply));
 bot.command('start', ({reply}) => {
     reply('Бот Профокма студентов РТУ МИРЭА. Бот работает в тестовом режиме.');
     mainMenu(reply);
@@ -163,93 +79,6 @@ bot.on('contact', ({reply, session, message}) => {
     session.tel = makeUpTel(message.contact.phone_number);
     requestKeyboard(reply);
 });
-// bot.on('contact', ({reply, session, message}) => {
-//     session.user = session.user || null;
-//     session.requestCounter = session.requestCounter || 0;
-//
-//     if (session.user.tel !== message.contact.phone_number && session.requestCounter <= 3) {
-//         session.user.tel = parseInt(message.contact.phone_number);
-//         session.requestCounter++;
-//
-//     }
-//
-//     if (session.user.tel == null) {
-//         sendError(reply);
-//         console.error('Value of session.userPhone is null');
-//
-//     } else {
-//         if (session.requestCounter > 3 || session.user.tel !== message.contact.phone_number) {
-//             return reply('Превышен лимит запросов разных номеров');
-//
-//         } else {
-//             request.get(`${api}chat/${message.chat.id}`, (err, res, body) => {
-//                 if (err) {
-//                     console.error(`${api}chat/${message.chat.id}\n ${err}`);
-//                     return sendError(reply);
-//                 }
-//
-//                 if (res.statusCode === 200) {
-//                     let data = JSON.parse(body);
-//
-//                 }
-//             });
-//         }
-//     }
-//
-//
-//     session.userPhone = session.userPhone || null;
-//     session.requestCounter = session.requestCounter || 0;
-//
-//     if (session.userPhone !== message.contact.phone_number && session.requestCounter <= 3) {
-//         session.userPhone = Number(message.contact.phone_number.replace('+', '').replace(/^8/, 7));
-//         session.requestCounter++;
-//
-//     }
-//
-//     if (session.userPhone == null) {
-//         sendError(reply);
-//         console.error('Value of session.userPhone is null');
-//
-//     } else {
-//         if (!(session.requestCounter <= 3 || session.userPhone === message.contact.phone_number)) {
-//             return reply('Превышен лимит запросов разных номеров');
-//         } else {
-//             request.get(`${api}chat/${message.chat.id}`, (err, res, body) => {
-//                 if (err) {
-//                     console.error(`${api}chat/${message.chat.id}\n ${err}`);
-//                     sendError(reply);
-//
-//                 }
-//
-//                 if (res.statusCode === 200) {
-//                     const data = JSON.parse(body);
-//                     if (data[1][0] === message.chat.id) { // надо еще проверять, что номер не  сохранен в сессии
-//                         if (data[1][1] === session.userPhone) { //
-//                             requestKeyboard(reply);
-//
-//                         } else {
-//                             updatePhone(reply, session, message);
-//
-//                         }
-//                     }
-//
-//                 } else if (res.statusCode === 404) {
-//                     updatePhone(reply, session, message);
-//
-//                 } else {
-//                     sendError(reply);
-//
-//                 }
-//             });
-//         }
-//     }
-// });
-
-bot.command('api', (ctx) => request(api, (error, res, body) => {
-    return ctx.reply(body).catch(err => console.error(err));
-}));
-
-bot.hears(/Мои данные/, ({reply, session}) => reply(session.tel || 'null'));
 
 bot.hears(/Основное меню/, ({reply}) => mainMenu(reply));
 
@@ -282,33 +111,6 @@ bot.hears(/(ржд|Ржд|РЖД) бонус/, ({match, reply, session}) => {
 
             }
         });
-    }
-});
-
-bot.hears(/Дотации/, ({match, reply, session}) => {
-    if (session.tel == null) {
-        reply('Сначала нужно отправить номер телефона');
-        return requestContact(reply);
-    } else {
-        request.get(`${api}user/${session.tel}`, (err, res, body) => {
-            if (err) {
-                console.error(`${api}chat/${message.chat.id}/tel/${session.tel}\n ${err}`);
-                return sendError(reply);
-
-            }
-
-            if (res.statusCode === 200) {
-                let data = JSON.parse(body)[0];
-                return reply(`${session.tel} ${data.status}`).catch(err => console.error(err));
-
-            } else if (res.statusCode === 404) {
-                return reply(`${session.tel} не найден в базе`);
-
-            } else {
-                return sendError(reply);
-
-            }
-        })
     }
 });
 
