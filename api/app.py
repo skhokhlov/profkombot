4c01a65a404e936cbf9ed55701d1aae0f5b25909
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2019-02-03 18:12:36 +0300

    Add subsidies and clean up

--- api/app.py
+++ api/app.py
@@ -9,48 +9,87 @@ from flask import Flask, make_response, abort, request
 app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379, db=0)
 
+
 class Tables(Enum):
     RZD = 'RZD'
+    SUBSIDIES = 'SUBSIDIES'
+
 
 def parse_number(tel):
     return re.sub(r'^8', '7', str(int(tel)))
 
+
+def response(data, status=200):
+    return make_response(json.dumps(data), status)
+
+
+def update_index(index, phone_numbers):
+    for tel in phone_numbers:
+        r.sadd(parse_number(tel), index)
+
+
+def check_in_index(index, tel):
+    return r.sismember(tel, index)
+
+
 @app.route('/api/v1/rzd/<int:user_phone>', methods=['GET'])
-def get_user(user_phone):
-    tel = parse_number(user_phone)
-    if r.sismember(tel, Tables.RZD.value):
-        return make_response(json.dumps(dict(zip(['rzd'], [True]))), 200)
+def get_rzd(user_phone):
+    if check_in_index(Tables.RZD.value, parse_number(user_phone)):
+        return response(dict(zip([Tables.RZD.value], [True])))
 
     else:
-        return make_response(json.dumps(dict(zip(['rzd'], [False]))), 404)
+        return response(dict(zip([Tables.RZD.value], [False])), 404)
+
+
+@app.route('/api/v1/subsidies/<int:user_phone>', methods=['GET'])
+def get_subsidies(user_phone):
+    if check_in_index(Tables.SUBSIDIES.value, parse_number(user_phone)):
+        return response(dict(zip([Tables.SUBSIDIES.value], [True])))
+
+    else:
+        return response(dict(zip([Tables.SUBSIDIES.value], [False])), 404)
+
 
 @app.route('/api/v1/rzd', methods=['PUT'])
-def db_update():
+def update_rzd():
     data = request.form.get('phone_numbers', False)
     if data:
-        for tel in ast.literal_eval(data):
-            r.sadd(parse_number(tel), Tables.RZD.value)
-
-        return make_response(json.dumps(dict(zip(['status'], ['OK']))), 200)
+        update_index(Tables.RZD.value, ast.literal_eval(data))
+        return response(dict(zip(['status'], ['OK'])))
 
     else:
-        return make_response(json.dumps(dict(zip(['status'], ['Bad Request']))), 400)
+        return response(dict(zip(['status'], ['Bad Request'])), 400)
+
+
+@app.route('/api/v1/subsidies', methods=['PUT'])
+def update_subsidies():
+    data = request.form.get('phone_numbers', False)
+    if data:
+        update_index(Tables.SUBSIDIES.value, ast.literal_eval(data))
+        return response(dict(zip(['status'], ['OK'])))
+
+    else:
+        return response(dict(zip(['status'], ['Bad Request'])), 400)
+
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return make_response(json.dumps(dict(zip(['status'], ['OK']))), 200)
+    return response(dict(zip(['status'], ['OK'])))
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(json.dumps(dict(zip(['status'], ['Not Found']))), 404)
+    return response(dict(zip(['status'], ['Not Found'])), 404)
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
-    return make_response(json.dumps(dict(zip(['status'], ['Method Not Allowed']))), 405)
+    return response(dict(zip(['status'], ['Method Not Allowed'])), 405)
+
 
 @app.errorhandler(500)
 def server_error(error):
-    return make_response(json.dumps(dict(zip(['status'], ['Server Error']))), 500)
+    return response(dict(zip(['status'], ['Server Error'])), 400)
 
 
 if __name__ == '__main__':
